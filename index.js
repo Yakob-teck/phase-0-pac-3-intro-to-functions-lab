a4f63ed80c036467b5b1593ecb10263a1b382467
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-09 16:41:54 -0700

    message

--- index.js
+++ index.js
@@ -14,11 +14,19 @@ function  logWhisper(string) {
   console.log(string.toLowerCase());
   return "YES INDEED!";
 }
-function  sayHiToHeadphonedRoommate(string){
-  shout(string);
-  return "I can't hear you!";
-  whisper(string);
+function  sayHiToHeadphonedRoommate(string) {
+  if (string==string.toLowerCase()) {
+    
+      return "I can\'t hear you!";
+    }
+      else if(string==string.toUpperCase()) {
+        return "YES INDEED!";
+      }
+      else if(string==="Let's have dinner together!") {
+        
+        return "I would love to!";
+  
+    }
+  }
+
  
-
-
-}
